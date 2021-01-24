2e3c229a877b98b968e9e18f56ad47846816a945
whosemario <gx546532416@gmail.com>
2021-01-24 21:39:03 +0800

    overlap的检测加上去了
    overlap的检测加上去了；同时可以画出layer1的格子了；性能很是问题

--- RecastDemo/Include/Sample_SVOMesh.h
+++ RecastDemo/Include/Sample_SVOMesh.h
@@ -3,6 +3,11 @@
 #include "Sample_TileMesh.h"
 #include <memory>
 #include <functional>
+#include <cstdlib>
+#include <vector>
+#include <set>
+
+using morton_code_t = std::uint64_t;
 
 struct FloatVector {
 public:
@@ -11,11 +16,37 @@ public:
 	float z = 0.0f;
 
 public:
-	FloatVector() {}
+	FloatVector() = default;
 	FloatVector(float inX, float inY, float inZ) :
 		x(inX), y(inY), z(inZ) {}
 	FloatVector(float inF):
 		x(inF), y(inF), z(inF) {}
+
+	FloatVector(FloatVector&) = default;
+
+	FloatVector& operator=(const FloatVector&) = default;
+
+	inline FloatVector operator+(const FloatVector& other) const {
+		return FloatVector(x + other.x, y + other.y, z + other.z);
+	}
+
+	inline FloatVector operator-(const FloatVector& other) const {
+		return FloatVector(x - other.x, y - other.y, z - other.z);
+	}
+
+	inline float cos(const FloatVector& other) const {
+		return x * other.x + y * other.y + z * other.z;
+	}
+
+	inline FloatVector operator*(float a) const {
+		return FloatVector(x * a, y * a, z *a);
+	}
+};
+
+struct FloatTri {
+public:
+	FloatVector indexes[3];
+	FloatVector normal;
 };
 
 using GetVolumeBoudingBoxFunc = std::function<bool(FloatVector&, FloatVector&)>;
@@ -29,17 +60,47 @@ public:
 public:
 	bool generate();
 
+	/*
+	* vector里面每两个FloatVector一组，记录的是box的最低最高三维点
+	*/
+	void getAllBlockedBox(std::vector<FloatVector>&) const;
+
+private:
 	void setupVolume() {
 		boundFunc_(origin_, extent_);
 	}
-	
+
+	/*
+	* 从 Layer1 到 LaynerN 标记某个体素 是否有 物体
+	* 这样预处理是为了加速后续的计算
+	*/
+	bool buildBlockedIndices();
+
+	/*
+	* (2^(voxelPower_-layer))^3
+	*/
+	inline std::int32_t getNodesInLayer(std::int32_t layer) const {
+		return static_cast<std::int32_t>(powf(powf(2, voxelPower_ - layer), 3));
+	}
+
+	/*
+	* extent.X * 2 / 2 ^ (voxelPower_ - layer)
+	*/
+	inline float getVoxelSize(std::int32_t layer) const {
+		return extent_.x * 2.0f / powf(2, voxelPower_ - layer);
+	}
+
+	void getPositionFromMortonCode(morton_code_t, FloatVector&, std::int32_t) const;
+
 private:
-	int voxelPowner_;
+	int voxelPower_;
 	GetVolumeBoudingBoxFunc boundFunc_;
 	OverlapBoxBlockingTestFunc checkFunc_;
 
 	FloatVector origin_;
 	FloatVector extent_;
+
+	std::vector<std::set<morton_code_t>> blockedIndices_;
 
 };
 using SVONVolumePtr = std::shared_ptr<SVONVolume>;
@@ -58,6 +119,7 @@ private:
 		return true;
 	}
 	bool overlapBoxBlockingText(const FloatVector&, float, int);
+	bool overlapBoxAndTri(const FloatVector&, float, const FloatTri&);
 
 private:
 	FloatVector origin_;
--- RecastDemo/Source/Sample_SVOMesh.cpp
+++ RecastDemo/Source/Sample_SVOMesh.cpp
@@ -14,6 +14,11 @@
 #include "Sample.h"
 #include "Sample_SVOMesh.h"
 
+#include "libmorton/morton.h"
+#include <cassert>
+
+#define INPUT_VOXEL_POWER 4 
+
 static int ceillog2(int x) {
 	static const int log_2[256] = {  /* log_2[i] = ceil(log2(i - 1)) */
 	  0,1,2,2,3,3,3,3,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,
@@ -32,17 +37,59 @@ static int ceillog2(int x) {
 }
 
 #define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
-#define LINE_WIDTH 5.0f
+#define LINE_WIDTH 2.0f
 #define LINE_COLOR_RED duRGBA(255, 0, 0, 128)
 
 SVONVolume::SVONVolume(int voxelPower, GetVolumeBoudingBoxFunc&& f1, OverlapBoxBlockingTestFunc&& f2)
-	: voxelPowner_(voxelPower), boundFunc_(std::move(f1)), checkFunc_(std::move(f2)) {}
+	: voxelPower_(voxelPower), boundFunc_(std::move(f1)), checkFunc_(std::move(f2)) {}
 
 bool SVONVolume::generate()
 {
 	setupVolume();
 
+	buildBlockedIndices();
+
 	return true;
+}
+
+void SVONVolume::getPositionFromMortonCode(morton_code_t mc, FloatVector& pos, std::int32_t layer) const {
+	std::uint32_t x, y, z;
+	morton3D_64_decode(mc, x, z, y);
+	auto sz = getVoxelSize(layer);
+	pos = origin_ - extent_ + FloatVector(x * sz, y * sz, z * sz) + FloatVector(sz * 0.5f);
+}
+
+bool SVONVolume::buildBlockedIndices()
+{
+	auto numLayers = voxelPower_ + 1;
+	blockedIndices_.clear();
+
+	auto nodesNum = getNodesInLayer(1);
+
+	blockedIndices_.push_back(std::set<morton_code_t>());
+	for (int mc = 0; mc < nodesNum; ++mc) {
+		FloatVector position;
+		getPositionFromMortonCode(mc, position, 1);
+		if (checkFunc_(position, getVoxelSize(1) * 0.5f, 1)) {
+			blockedIndices_[0].insert(static_cast<morton_code_t>(mc));
+		}
+	}
+	return true;
+}
+
+void SVONVolume::getAllBlockedBox(std::vector<FloatVector>& out) const {
+
+	std::int32_t layer = 1;
+	for (auto& mc_set : blockedIndices_) {
+		for (auto mc : mc_set) {
+			FloatVector sz(getVoxelSize(layer) * 0.5f);
+			FloatVector position;
+			getPositionFromMortonCode(mc, position, layer);
+			out.push_back(position - sz);
+			out.push_back(position + sz);
+		}
+		layer += 1;
+	}
 }
 
 void Sample_SVOMesh::handleRender()
@@ -55,11 +102,25 @@ void Sample_SVOMesh::handleRender()
 	if (!hasBuild_) return;
 
 	// 1. 画最上层的那个立方体
-	duDebugDrawBoxWire(
+	/*duDebugDrawBoxWire(
 		&m_dd, 
 		origin_.x - extent_.x, origin_.y - extent_.y, origin_.z - extent_.z, 
 		origin_.x + extent_.x, origin_.y + extent_.y, origin_.z + extent_.z, 
-		LINE_COLOR_RED, LINE_WIDTH);
+		LINE_COLOR_RED, LINE_WIDTH);*/
+
+	// 2. 画 blocked indices 相关的 box
+	std::vector<FloatVector> blockedBox;
+	vol_->getAllBlockedBox(blockedBox);
+
+	for (auto i = 0; i < blockedBox.size(); i += 2) {
+		auto& minBox = blockedBox[i];
+		auto& maxBox = blockedBox[i + 1];
+		duDebugDrawBoxWire(
+			&m_dd,
+			minBox.x, minBox.y, minBox.z,
+			maxBox.x, maxBox.y, maxBox.z,
+			LINE_COLOR_RED, LINE_WIDTH);
+	}
 }
 
 bool Sample_SVOMesh::handleBuild()
@@ -81,7 +142,7 @@ bool Sample_SVOMesh::handleBuild()
 
 	if (!vol_) {
 		vol_ = std::make_shared<SVONVolume>(
-			3, 
+			INPUT_VOXEL_POWER, 
 			std::bind(&Sample_SVOMesh::getVolumeBoundingBox, this, std::placeholders::_1, std::placeholders::_2),
 			std::bind(&Sample_SVOMesh::overlapBoxBlockingText, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)
 		);
@@ -95,5 +156,91 @@ bool Sample_SVOMesh::handleBuild()
 }
 
 bool Sample_SVOMesh::overlapBoxBlockingText(const FloatVector& pos, float boxRadius, int layers) {
+	const auto* mesh = m_geom->getMesh();
+	const auto* tris = mesh->getTris();
+	const auto* verts = mesh->getVerts();
+	const auto* norm = mesh->getNormals();
+	for (int i = 0; i < mesh->getTriCount() * 3; i += 3) {
+		FloatTri tri;
+		tri.normal = FloatVector(norm[i], norm[i+1], norm[i+2]);
+		for (int j = 0; j < 3; ++j) {
+			const auto* v = &verts[tris[i + j] * 3];
+			tri.indexes[j] = FloatVector(v[0], v[1], v[2]);
+		}
+		if (overlapBoxAndTri(pos, boxRadius, tri)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+#define DIRSIZE 8
+static FloatVector BOX_DIR[DIRSIZE] = {
+	{-1, -1, -1},
+	{-1, -1,  1},
+	{-1,  1,  1},
+	{ 1,  1,  1},
+	{ 1,  1, -1},
+	{ 1, -1, -1},
+	{-1,  1, -1},
+	{ 1, -1,  1}
+};
+
+static const float DELTA = 0.001f;
+
+bool Sample_SVOMesh::overlapBoxAndTri(const FloatVector& pos, float boxRadius, const FloatTri& tri) {
+	// 1. 先检查立方体所有的点是否都在三角面片的同一面
+	int flag = -1;	// -1=N/A, 1=小于0，2大于0
+	int i = 0;
+	for (; i < DIRSIZE; ++i) {
+		auto v = (pos + BOX_DIR[i] * boxRadius - tri.indexes[0]).cos(tri.normal);
+		if (v > DELTA) {
+			if (flag == -1) flag = 2;
+			else if (flag == 1) break;	// maybe overlap
+		}
+		else if (v < -DELTA) {
+			if (flag == -1) flag = 1;
+			else if (flag == 2) break;
+		}
+	}
+	assert(flag != -1);
+	if (i == DIRSIZE) return false;
+	// 2. 检查三角形三个顶点是否在某一个面的同一边
+	// +x
+	float t = pos.x + boxRadius;
+	if (tri.indexes[0].x - t > DELTA && 
+		tri.indexes[1].x - t > DELTA && 
+		tri.indexes[2].x - t > DELTA)
+		return false;
+	// -x
+	t = pos.x - boxRadius;
+	if (t - tri.indexes[0].x > DELTA &&
+		t - tri.indexes[1].x > DELTA &&
+		t - tri.indexes[2].x > DELTA)
+		return false;
+	// +y
+	t = pos.y + boxRadius;
+	if (tri.indexes[0].y - t > DELTA &&
+		tri.indexes[1].y - t > DELTA &&
+		tri.indexes[2].y - t > DELTA)
+		return false;
+	// -y
+	t = pos.y - boxRadius;
+	if (t - tri.indexes[0].y > DELTA &&
+		t - tri.indexes[1].y > DELTA &&
+		t - tri.indexes[2].y > DELTA)
+		return false;
+	// +z
+	t = pos.z + boxRadius;
+	if (tri.indexes[0].z - t > DELTA &&
+		tri.indexes[1].z - t > DELTA &&
+		tri.indexes[2].z - t > DELTA)
+		return false;
+	// -z
+	t = pos.z - boxRadius;
+	if (t - tri.indexes[0].z > DELTA &&
+		t - tri.indexes[1].z > DELTA &&
+		t - tri.indexes[2].z > DELTA)
+		return false;
 	return true;
 }
